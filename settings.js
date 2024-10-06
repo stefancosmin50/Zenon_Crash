5e60f19743218a928c341d16849e0b56b0bc9e55
cosmin <scosmin764@gmail.com>
2024-10-06 20:48:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48459439078"
+global.ownernomer = "0770811929"
 global.ownername = "Toxxic Md"
 global.ytname = "Toxxic Md"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '48459439078'  //creator number
+global.ownernumber = '0770811929'  //creator number
 global.ownername = 'Toxxic Md' //owner name
 global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'ZENON\n\nContact: 48459439078'
+global.author = 'ZENON\n\nContact: 0770811929'
 
 //console view/theme
 global.themeemoji = '💀'
